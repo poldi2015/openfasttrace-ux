761e1b33cd3575ee22bf77655e1b99a95b9f61bd
Bernd Haberstumpf <poldi@thatswing.de>
2024-12-12 23:38:25 +0100

    Activate logging by default

--- test/main/js/utils/log.test.ts
+++ test/main/js/utils/log.test.ts
@@ -1,18 +1,18 @@
 import {describe, expect} from "vitest";
-import {hiddenLogTags, knownLogTags, Log, log, resetLogTags, showLogTags} from "@main/utils/log";
+import {hiddenLogTags, hideLogTags, knownLogTags, Log, log, resetLogTags, showLogTags} from "@main/utils/log";
 import {test} from "../fixtures/fixtures";
 
 describe("Tests of the Logger API", () => {
     test("Write a single log message", ({logs}) => {
         log.info('test', "Hallo");
-        expect(logs).toHaveReturnedWith('test Hallo')
+        expect(logs).toHaveReturnedWith('test Hallo');
     });
 
-    test("Use a tagged Logger, tag by default is hidden", ({logs}) => {
+    test("Use a tagged Logger, tag by default is shown", ({logs}) => {
         const log = new Log("Tag");
-        expect(hiddenLogTags()).toEqual(["Tag"]);
+        expect(knownLogTags()).toEqual(["Tag"]);
         log.info('test', "Hallo");
-        expect(logs).toHaveLength(0);
+        expect(logs).toHaveBeenCalled();
     });
 
     test("Use a tagged Logger, enable all tags", ({logs}) => {
@@ -25,6 +25,7 @@ describe("Tests of the Logger API", () => {
     test("Use a tagged Logger, enable selected tags", ({logs}) => {
         const log1 = new Log("Tag1");
         const log2 = new Log("Tag2");
+        hideLogTags();
         showLogTags("Tag1");
         log1.info('test', "Hallo1");
         log2.info('test', "Hallo2");
@@ -36,7 +37,7 @@ describe("Tests of the Logger API", () => {
     test("Two tagged loggers have applied two hidden logs", ({logs}) => {
         new Log("Tag1");
         new Log("Tag2");
-        expect(hiddenLogTags()).toEqual(["Tag1","Tag2"]);
+        expect(hiddenLogTags()).toEqual([]);
     });
 
     test("Two tagged loggers part of all log Tags", ({logs}) => {
