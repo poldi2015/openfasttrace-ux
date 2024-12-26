69bd0de113f9af4835eebed40797de426afba16c
Bernd Haberstumpf <poldi@thatswing.de>
2024-12-26 12:50:11 +0100

    Added licenses

--- src/main/js/controller/oft_state_builder.ts
+++ src/main/js/controller/oft_state_builder.ts
@@ -1,3 +1,22 @@
+/*
+  OpenFastTrace UX
+
+ Copyright (C) 2024-2025 itsallcode.org, Bernd Haberstumpf
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public
+ License along with this program.  If not, see
+ <http://www.gnu.org/licenses/gpl-3.0.html>.
+*/
 import {CoverType, FilterName, OftState, SelectedFilterIndexes} from "@main/model/oft_state";
 import {FilterModels} from "@main/model/filter";
 import {SpecItem} from "@main/model/specitems";
@@ -13,11 +32,9 @@ export class OftStateBuilder {
             this.oftState.selectedIndex = null;
             this.oftState.selectedPath = [];
         }
-        /*
         Object.entries(metaModel).forEach(([filterName, _]: [string, any]) => {
             this.oftState.selectedFilters.set(filterName, []);
         });
-         */
 
         return this;
     }
