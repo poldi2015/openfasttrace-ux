80e6e2150ebfcb9ccd81b50f17bbd2130940ffc1
Bernd Haberstumpf <poldi@thatswing.de>
2024-12-28 17:55:40 +0100

    Reset HTML before each test execution

--- test/main/js/fixtures/dom.ts
+++ test/main/js/fixtures/dom.ts
@@ -17,7 +17,7 @@
  License along with this program.  If not, see
  <http://www.gnu.org/licenses/gpl-3.0.html>.
 */
-import {expect} from "vitest";
+import {beforeEach, expect} from "vitest";
 import jQuery from "jquery";
 
 declare module 'vitest' {
@@ -38,6 +38,10 @@ declare global {
  */
 export const $: JQueryStatic = jQuery;
 window.$ = $;
+
+beforeEach(() => {
+    document.body.innerHTML = '';
+})
 
 
 /**
