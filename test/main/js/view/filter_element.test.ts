71b1226920761a48acb0f9caa04129c383952ac1
Bernd Haberstumpf <poldi@thatswing.de>
2024-12-29 20:20:03 +0100

    Added a matcher to filter_element.test.ts

--- test/main/js/view/filter_element.test.ts
+++ test/main/js/view/filter_element.test.ts
@@ -1,16 +1,33 @@
-import {describe} from "vitest";
+import {describe, expect} from "vitest";
 import {test} from "@test/fixtures/fixtures";
 import {FilterElement} from "@main/view/filter_element";
 import {OftStateBuilder} from "@main/controller/oft_state_builder";
 import {OftStateController} from "@main/controller/oft_state_controller";
 import {$} from "@test/fixtures/dom";
 import {FILTER_MODELS_SAMPLE} from "@test/mocks/filter_mocks";
-import {ExpandableElements} from "../../../../src/main/js/view/expandable_elements";
+import {ExpandableElements} from "@main/view/expandable_elements";
 
 const HTML_MODEL = `
 <div class="expandable" data-title="Type" data-tooltip="Type part of the Requirement ID">
     <select class="filter" id="type"></select>
 </div>
+`;
+
+const GOLDEN_SAMPLE_FILTER_MODEL = `
+<div class="expandable" data-title="Type" data-tooltip="Type part of the Requirement ID">
+    <div class="_expandable-header">
+        <span>Type</span>
+        <div class="filter-buttons">
+            <a href="#">All</a>
+            <a href="#">Off</a>
+        </div>        
+    </div>
+    <div class="_expandable-content visible">
+        <select class="filter" id="type" multiple="multiple" size="1" disabled="disabled">
+            <option id="type_0" style="color:red" class="filter-item-off">Feature&nbsp;&nbsp;(5)</option>
+        </select>
+    </div>
+</div>    
 `;
 
 describe("Tests  for FilterElement", () => {
@@ -25,6 +42,6 @@ describe("Tests  for FilterElement", () => {
         const oftState: OftStateController = new OftStateController(oftStateBuilder.build());
         const filterElement = new FilterElement("type", selectElement, FILTER_MODELS_SAMPLE["type"], oftState);
         filterElement.init();
-        console.log(body.html());
+        expect(body).toMatchHTML(GOLDEN_SAMPLE_FILTER_MODEL);
     });
 });
