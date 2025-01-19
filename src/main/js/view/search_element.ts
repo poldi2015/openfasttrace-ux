5fafbb44cf14febf70af5cfd900677f4c11003c1
Bernd Haberstumpf <poldi@thatswing.de>
2025-01-20 00:56:44 +0100

    Deferred search entry. added comments to search field.

--- src/main/js/view/search_element.ts
+++ src/main/js/view/search_element.ts
@@ -3,6 +3,7 @@ import {Log} from "@main/utils/log";
 import {Filter, NameFilter, NameFilterTarget} from "@main/model/filter";
 import {FilterName} from "@main/model/oft_state";
 import {ChangeEvent, ChangeListener, EventType} from "@main/model/change_event";
+import {Deferred} from "@main/utils/async";
 
 export class SearchElement {
     constructor(
@@ -16,10 +17,12 @@ export class SearchElement {
     private inputElement: HTMLInputElement | null = null;
 
     private filterOrFocusChangeListener: ChangeListener = (event: ChangeEvent): void => {
+        this.log.info("filterOrFocusChangeListener", event.oftState);
         event.handleFilterChange((filters, _) => this.filterChanged(filters));
     }
 
     public init(): SearchElement {
+        this.log.info("init");
         this.addSearchForm();
         this.deactivate();
         return this;
@@ -29,15 +32,17 @@ export class SearchElement {
      * Enable the search element, ready to be used in the UI.
      */
     public activate(): void {
+        this.log.info("activate");
         this.oftState.addChangeListener(this.filterOrFocusChangeListener, EventType.Filters); // Also covers focus Change
-        if (this.inputElement) this.inputElement.disabled = false;
+        if (this.inputElement != null) this.inputElement.disabled = false;
     }
 
     /**
      * Deactivates the search element, making it unavailable in the UI.
      */
     public deactivate(): void {
-        if (this.inputElement) this.inputElement.disabled = true;
+        this.log.info("deactivate");
+        if (this.inputElement != null) this.inputElement.disabled = true;
         this.oftState.removeChangeListener(this.filterOrFocusChangeListener);
     }
 
@@ -56,19 +61,20 @@ export class SearchElement {
         `);
         const input: JQuery<HTMLInputElement> = searchForm.find("form input[type='text']") as JQuery<HTMLInputElement>;
         this.inputElement = input[0];
+        const deferred = new Deferred(300);
         input.on('keyup', (event) => {
             event.preventDefault();
-            this.inputChanged((event.target as HTMLInputElement).value);
+            deferred.run(() => this.notifyInputChanged((event.target as HTMLInputElement).value));
         });
 
         const clear: JQuery<HTMLInputElement> = searchForm.find("form button._search-clear") as JQuery<HTMLInputElement>;
         clear.on('click', (event) => {
             event.preventDefault();
-            this.inputChanged("");
+            this.notifyInputChanged("");
         });
     }
 
-    private inputChanged(value: string): void {
+    private notifyInputChanged(value: string): void {
         let regEx = false;
         let target: NameFilterTarget = NameFilterTarget.name;
         if (value.startsWith("+")) {
@@ -78,6 +84,7 @@ export class SearchElement {
         if (!value.match(/^[a-zA-Z0-9_-]*$/)) {
             regEx = true;
         }
+        this.log.info("notifyInputChanged value", value, "regEx", regEx, "target", target);
         const filters: Map<FilterName, Filter> = new Map([[NameFilter.FILTER_NAME, new NameFilter(value, regEx, target)]]);
         this.oftState.selectFilters(filters);
     }
@@ -90,6 +97,7 @@ export class SearchElement {
      * @param selectedFilters filters to be selected (includes the selection of all filters not only this one)
      */
     private filterChanged(selectedFilters: Map<FilterName, Filter>): void {
+        this.log.info("filterChanged filters", selectedFilters);
         if (!selectedFilters.has(NameFilter.FILTER_NAME)) {
             this.changeValue("");
         } else {
@@ -99,6 +107,7 @@ export class SearchElement {
 
     private changeValue(value: string): void {
         const currentValue: string | undefined = this.inputElement?.value.startsWith("+") ? this.inputElement?.value.substring(5).trimStart() : this.inputElement?.value;
+        this.log.info("changeValue from", currentValue, "to", value);
         if (this.inputElement != null && value != currentValue) this.inputElement.value = value;
     }
 
