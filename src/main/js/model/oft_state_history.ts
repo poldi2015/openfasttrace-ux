55491c1d7e8dce848c33b3eec6a6749b4cb98d6f
Bernd Haberstumpf <poldi@thatswing.de>
2025-01-20 00:55:23 +0100

    Fixed limitHistorySize, added comments and log messages, fixel toPreviousState

--- src/main/js/model/oft_state_history.ts
+++ src/main/js/model/oft_state_history.ts
@@ -32,14 +32,18 @@ export class OftStateHistory {
     /**
      * Initializes the eventFactories.
      *
-     * @param changeEvent The event that created the initial state
+     * @param changeEvent The event that created the initial state of the history does not contain one
      */
     public init(changeEvent: ChangeEvent) {
-        this.log.info("init", this.history.length, this.currentPosition);
+        this.log.info("init length", this.history.length, "currentPosition", this.currentPosition);
         if (this.history.length == 0) {
             this.log.info("Adding initial state", changeEvent);
             this.history.push(new HistoryItem(changeEvent));
         }
+    }
+
+    public length(): number {
+        return this.history.length;
     }
 
     /**
@@ -50,11 +54,10 @@ export class OftStateHistory {
      * @return the added {@link HistoryItem}
      */
     public pushEvent(changeEvent: ChangeEvent, position: number = this.currentPosition): ChangeEvent {
-        this.log.info("pushState", position, changeEvent);
+        this.log.info("pushState position", position, "event", changeEvent);
         const historyItem: HistoryItem = new HistoryItem(changeEvent);
         this.history.splice(position, 0, historyItem);
-        this.limitHistorySize(position);
-        this.currentPosition = position;
+        this.currentPosition = this.limitHistorySize(position);
         return changeEvent;
     }
 
@@ -64,7 +67,8 @@ export class OftStateHistory {
      * @param position the position. default to the last added
      * @return the pulled {@link ChangeEvent}
      */
-    public pullState(position: number = this.currentPosition): ChangeEvent {
+    public pullState(position: number = this.currentPosition): ChangeEvent | null {
+        if (position < 0 || position + 1 >= this.history.length) return null;
         if (this.history.length == 1) {
             this.currentPosition = 0;
             return this.history[0].changeEvent;
@@ -75,17 +79,28 @@ export class OftStateHistory {
     }
 
     /**
+     * Peeks (not changing the history) the {@link ChangeEvent} at the given position in the history.
+     *
+     * @param position the position. default to the last added
+     * @return the {@link ChangeEvent} or null of position is out of range
+     */
+    public peekState(position: number = this.currentPosition): ChangeEvent | null {
+        return position >= 0 || position < this.history.length ? this.history[position].changeEvent : null;
+    }
+
+    /**
      * switches the history to the previous (older) entry and returns a {@link ChangeEvent} representing the state change.
      *
      * @return {@link ChangeEvent} executing the state change or null if in the latest state
      */
     public toPreviousState(): ChangeEvent | null {
-        if (this.currentPosition >= this.history.length) return null;
+        if ((this.currentPosition + 1) >= this.history.length) return null;
         const fromEntry: HistoryItem = this.history[this.currentPosition];
+        this.log.info("history", this.history);
         this.currentPosition++;
         const toEntry: HistoryItem = this.history[this.currentPosition];
         const changeEvent: ChangeEvent = new ChangeEvent(fromEntry.changeEvent.types, toEntry.changeEvent.oftState);
-        this.log.info("toPreviousState", this.currentPosition, this.history.length, changeEvent);
+        this.log.info("toPreviousState position", this.currentPosition, "length", this.history.length, "event", changeEvent);
         return changeEvent;
     }
 
@@ -95,10 +110,10 @@ export class OftStateHistory {
      * @return {@link ChangeEvent} executing the state change or null if in the latest state
      */
     public toNextState(): ChangeEvent | null {
-        if (this.currentPosition < 1) return null
+        if (this.currentPosition < 1) return null;
         this.currentPosition--;
         const changeEvent: ChangeEvent = this.history[this.currentPosition].changeEvent;
-        this.log.info("toNextState", this.currentPosition, this.history.length, changeEvent);
+        this.log.info("toNextState position", this.currentPosition, "history", this.history.length, "event", changeEvent);
         return changeEvent;
     }
 
@@ -107,9 +122,15 @@ export class OftStateHistory {
      * dependent on index being in the last or in the first halve of the history.
      *
      * @param index where a new element was added
+     * @return newIndex
      */
-    private limitHistorySize(index: number): void {
-        if (this.history.length > this.maxSize) this.history.splice(index >= (this.maxSize / 2) ? 0 : -1, 1);
+    private limitHistorySize(index: number = this.currentPosition): number {
+        if (this.history.length > this.maxSize) {
+            const removeFirst: boolean = index >= (this.maxSize / 2);
+            this.history.splice(removeFirst ? 0 : -1, 1);
+            if (removeFirst) index--;
+        }
+        return index;
     }
 
 } // OftStateHistory
