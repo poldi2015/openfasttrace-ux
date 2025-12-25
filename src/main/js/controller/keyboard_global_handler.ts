58718f0d260f1380adef69ba1ca1a21d45642352
Bernd Haberstumpf <poldi@thatswing.de>
2025-12-25 15:05:57 +0100

    Added search to keyboard handling. Commented out Filters as the keyboard support for them are not yet implemented.

--- src/main/js/controller/keyboard_global_handler.ts
+++ src/main/js/controller/keyboard_global_handler.ts
@@ -3,19 +3,29 @@ import {Log} from "@main/utils/log";
 
 const SPECITEMS_ELEMENT_ID = "#specitems";
 const SIDEBAR_LEFT_ID = "#select-type";
+const SEARCH_ID = "#search-input";
 
 export class KeyboardGlobalHandler extends KeyboardHandler {
 
     constructor() {
         super([], [
-                new Key("1", (_) => this.focusFilters()),
-                new Key("2", (_) => this.focusSpecItems())
+                //new Key("1", (_) => this.focusFilters()),
+                new Key("2", (_) => this.focusSpecItems()),
+                new Key("f", (_) => this.search()),
+                new Key("F", (_) => this.search()),
+                new Key("/", (_) => this.search()),
             ],
             new Log("KeyboardGlobalController"));
     }
 
     private readonly sidebarLeftElement: JQuery = $(SIDEBAR_LEFT_ID);
     private readonly specItemsElements: JQuery = $(SPECITEMS_ELEMENT_ID);
+    private readonly searchElements: JQuery = $(SEARCH_ID);
+
+    public activate(): void {
+        super.activate();
+        this.focusSpecItems();
+    }
 
 
     //
@@ -27,6 +37,11 @@ export class KeyboardGlobalHandler extends KeyboardHandler {
 
     private focusSpecItems(): boolean {
         return this.focus(this.specItemsElements);
+    }
+
+    private search(): boolean {
+        this.log.info("search");
+        return this.focus(this.searchElements);
     }
 
 } // KeyboardGlobalController
